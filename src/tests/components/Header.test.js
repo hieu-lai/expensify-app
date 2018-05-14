ec967cdd4b97c4b97f577fba35471312849e01bf
Hieu Lai <hieuqlai@gmail.com>
2018-05-14 23:13:17 +1000

    added privateroute

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 import { shallow } from 'enzyme';
 
+let wrapper, startLogout;
+
+beforeEach(() => {
+  startLogout = jest.fn();
+  wrapper = shallow(<Header startLogout={startLogout}/>);
+});
+
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
